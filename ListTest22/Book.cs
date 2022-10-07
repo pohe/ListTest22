8c9d73f9e767b94b5bdd94d7acaf3471fe693cf8
Poul Henriksen <pohe@easj.dk>
2022-10-07 13:56:09 +0200

    Add project files.

---
+++ ListTest22/Book.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ListTest22
+{
+    public class Book
+    {
+
+        public string Title { get; set; }
+        public string Author { get; set; }
+        public string Isbn { get; set; }
+
+        public Book(string titel, string author, string isbn)
+        {
+            Title = titel;
+            Author = author;
+            Isbn = isbn;
+        }
+
+
+        public override string ToString()
+        {
+            return $"{Title} {Author} {Isbn}";
+        }
+    }
+}
---
+++ ListTest22/Program.cs
@@ -0,0 +1,41 @@
+﻿// See https://aka.ms/new-console-template for more information
+//Console.WriteLine("Hello, World!");
+using ListTest22;
+
+List<int> myFirstList = new List<int> ();
+myFirstList.Add (34);
+myFirstList.Add(-233);//tilføjer bagerst
+myFirstList.Add(9801);
+myFirstList.Add(67);
+myFirstList.Add(2);
+myFirstList.Add(-9582);
+myFirstList.Add(770);
+myFirstList.Insert(3, 1300);//indsætter på position og rykker de andre
+myFirstList[4] = 85;
+
+myFirstList.Remove(9801);
+
+//foreach (int item in myFirstList)
+//{
+//    Console.WriteLine(item);
+//}
+
+
+List<Book> books = new List<Book>();
+
+Book b1 = new Book("C# bogen", "John Sharp", "124124124");
+Book b2 = new Book("PHP bogen", "John Jensen", "sdfsf");
+Book b3 = new Book("Pascal bogen", "John Pascal", "76767");
+
+books.Add(b1);
+books.Add(b2);
+books.Add(b3);
+
+foreach(Book b in books)
+{
+    Console.WriteLine(b);
+}
+
+books.Remove(b2);
+
+
